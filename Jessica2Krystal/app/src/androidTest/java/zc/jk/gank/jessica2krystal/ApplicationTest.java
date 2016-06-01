a1b7e2cd7e77eb4de7c4a13aa268a53af0843f43
ZhangCheng <1069441081@qq.com>
2016-06-01 16:01:03 +0800

    创建项目

---
+++ Jessica2Krystal/app/src/androidTest/java/zc/jk/gank/jessica2krystal/ApplicationTest.java
@@ -0,0 +1,13 @@
+package zc.jk.gank.jessica2krystal;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ Jessica2Krystal/app/src/main/java/zc/jk/gank/jessica2krystal/MainActivity.java
@@ -0,0 +1,13 @@
+package zc.jk.gank.jessica2krystal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
